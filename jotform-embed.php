2512fb9f761d28ec89e5be3aaeb73aef5a1b9456
egebagcivan <ege.bagcivan.98@gmail.com>
2022-06-24 16:35:34 +0300

    Menu Items added

--- jotform-embed.php
+++ jotform-embed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Jotform Drag & Drop Form Builder
  * Plugin URI:        https://jotform.com
- * Description:       Create Forms Using Jotform
+ * Description:       Create Forms Using Jotform v1
  * Author:            Jotform
  * Author URI:        https://jotform.com
  * Version:           1.0
@@ -10,3 +10,35 @@
  * You should have received a copy of the GNU General Public License
  * along with Jotform. If not, see <https://www.gnu.org/licenses/>.
  */
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! defined( 'JOTFORM_VERSION' ) ) {
+	/**
+	 * Plugin version.
+	 *
+	 * @since 1.0.0
+	 */
+	define( 'JOTFORM_VERSION', '1.0' );
+}
+
+// Plugin Folder Path.
+if ( ! defined( 'JOTFORM_PLUGIN_DIR' ) ) {
+	define( 'JOTFORM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+// Plugin Folder URL.
+if ( ! defined( 'JOTFORM_PLUGIN_URL' ) ) {
+	define( 'JOTFORM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+// Plugin Root File.
+if ( ! defined( 'JOTFORM_PLUGIN_FILE' ) ) {
+	define( 'JOTFORM_PLUGIN_FILE', __FILE__ );
+}
+
+
+  // Admin/Dashboard only includes, also in ajax.
+    require_once JOTFORM_PLUGIN_DIR . 'includes/admin/class-menu.php';
+
