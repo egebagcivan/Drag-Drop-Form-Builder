70e95e10073fcd009bcee94b7b47f38ae82d1bbd
egebagcivan <ege.bagcivan.98@gmail.com>
2022-06-24 15:55:41 +0300

    New Plugin Created

---
+++ jotform-embed.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Jotform Drag & Drop Form Builder
+ * Plugin URI:        https://jotform.com
+ * Description:       Create Forms Using Jotform
+ * Author:            Jotform
+ * Author URI:        https://jotform.com
+ * Version:           1.0
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jotform. If not, see <https://www.gnu.org/licenses/>.
+ */
