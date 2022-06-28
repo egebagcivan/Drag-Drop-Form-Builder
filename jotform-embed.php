5cf23410f1e7166c8bb28a39a34d38c52d1a221c
egebagcivan <ege.bagcivan.98@gmail.com>
2022-06-28 14:09:08 +0300

    Structure Change
    Added assets folder and made the menu icon dynamic to change it easily

--- jotform-embed.php
+++ jotform-embed.php
@@ -14,31 +14,31 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-if ( ! defined( 'JOTFORM_VERSION' ) ) {
+//tanımlayıcı isimler koyulacak
+if ( ! defined( 'JOTFORM_DRAGDROP_FORM_VERSION' ) ) {
 	/**
 	 * Plugin version.
 	 *
 	 * @since 1.0.0
 	 */
-	define( 'JOTFORM_VERSION', '1.0' );
+	define( 'JOTFORM_DRAGDROP_FORM_VERSION', '1.0' );
 }
 
+/* PATH DEFINE */
 // Plugin Folder Path.
-if ( ! defined( 'JOTFORM_PLUGIN_DIR' ) ) {
-	define( 'JOTFORM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+if ( ! defined( 'JOTFORM_DRAGDROP_FORM_PLUGIN_DIR' ) ) {
+	define( 'JOTFORM_DRAGDROP_FORM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 // Plugin Folder URL.
-if ( ! defined( 'JOTFORM_PLUGIN_URL' ) ) {
-	define( 'JOTFORM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+if ( ! defined( 'JOTFORM_DRAGDROP_FORM_PLUGIN_URL' ) ) {
+	define( 'JOTFORM_DRAGDROP_FORM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
-
 // Plugin Root File.
-if ( ! defined( 'JOTFORM_PLUGIN_FILE' ) ) {
-	define( 'JOTFORM_PLUGIN_FILE', __FILE__ );
+if ( ! defined( 'JOTFORM_DRAGDROP_FORM_PLUGIN_FILE' ) ) {
+	define( 'JOTFORM_DRAGDROP_FORM_PLUGIN_FILE', __FILE__ );
 }
 
-
+//yapı akismet e yakın olmalı
   // Admin/Dashboard only includes, also in ajax.
-    require_once JOTFORM_PLUGIN_DIR . 'includes/admin/class-menu.php';
+    require_once JOTFORM_DRAGDROP_FORM_PLUGIN_DIR . 'includes/class-menu.php';
 
